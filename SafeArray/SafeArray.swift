9792ffdfda27052e5ab00cd1e4859f5652b1a570
bloodysnowx <bloodysnowx@gmail.com>
2016-08-24 14:51:23 +0900

    fix

--- SafeArray/SafeArray.swift
+++ SafeArray/SafeArray.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension Array {
-    subscript (safe index: Int) -> Element? {
+    public subscript (safe index: Int) -> Element? {
         get { return indices ~= index ? self[index] : nil }
     }
 }			
