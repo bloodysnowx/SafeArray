746cfc447fdf6559f83e2b8f3702209fcbef9d5e
bloodysnow <bloodysnowx@gmail.com>
2016-09-18 22:52:43 +0900

    ready for carthage

---
+++ SafeArray/SafeArray.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeArray.
+FOUNDATION_EXPORT double SafeArrayVersionNumber;
+
+//! Project version string for SafeArray.
+FOUNDATION_EXPORT const unsigned char SafeArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeArray/PublicHeader.h>
+
+
